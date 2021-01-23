f6ccb9baa75de8a55007f2959aa78b1ccfffe369
Kawatsu Kenya <3982ne@gmail.com>
2021-01-23 19:02:14 +0900

    implement notification for slack

--- lambda/index.js
+++ lambda/index.js
@@ -1,53 +1,46 @@
-const AWS = require('aws-sdk')
-const https = require('https');
+const AWS = require("aws-sdk");
+const https = require("https");
+const fetch = require("node-fetch");
+const util = require("util");
+const { promisify } = util;
 
-const ssm = new AWS.SSM();
+const SSM_VERSION = "2014-11-06";
+
+const ssm = new AWS.SSM({ apiVersion: SSM_VERSION });
 
 exports.handler = async (event) => {
-  return new Promise((resolve, reject) => {
-    var params = {
-      Names: [
-        '/prd/write-code-everyday/slack-webhook-url'
-      ],
-      WithDecryption: true
+  return new Promise(async (resolve, reject) => {
+    const parameterNames = {
+      Names: ["/prd/write-code-everyday/slack-webhook-url"],
+      WithDecryption: true,
     };
-    ssm.getParameters(params, function(err, data) {
-      console.log('[ssm#getParamerters', data)
-      if (err) {
-        console.error(err, err.stack);
-        return reject(err)
-      }
+    const parameters = await ssm
+      .getParameters(parameterNames)
+      .promise()
+      .catch((e) => console.error(e));
+    console.log("[ssm#getParamerters", parameters.Parameters);
+    const [webHookUrlParam] = parameters.Parameters;
 
-      const meta = JSON.stringify({
-        username: 'node_bot',
-        text: 'write code',
-        icon_emoji: ':sunglasses:',
-      });
-      const options = {
-        hostname: 'hooks.slack.com',
-        port: 443,
-        path: '',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': Buffer.byteLength(meta),
-        },
-      };
+    console.log("----------------params---------------");
+    console.log(parameterNames);
 
-      const req = https.request(options, (res) => {
-        if (res.statusCode === 200) {
-          return console.log('[request#success]' + res.statusCode);
-        }
-        console.log('[request#failed]' + res.statusCode);
-      });
+    const data = {
+      username: "write-code-everyday",
+      text: "write code",
+      icon_emoji: ":fire:",
+    };
 
-      req.on('error', (e) => {
-        console.error(e);
-      });
-
-      req.write(data);
-      req.end();
-      resolve();
+    const res = await fetch(webHookUrlParam.Name, {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
     });
+    if (!res.ok) {
+      console.error(res);
+    }
+    console.log(res);
+    const json = await res.json();
+
+    console.log(json);
   });
-}
+};
